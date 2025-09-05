5245e6dc1ec05866c7f0d32946db5912b4305b30
paingthu555 <paingtnc@gmail.com>
2025-09-05 17:06:44 +0630

    dev: code successful but need to remove unnecessary code

--- iam.tf
+++ iam.tf
@@ -54,7 +54,30 @@ resource "aws_iam_role_policy_attachment" "lambda_exec_policy_attachment" {
   role       = aws_iam_role.lambda_exec_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_schedule_policy_attachment" {
-  policy_arn = aws_iam_policy.policy.arn
+# resource "aws_iam_role_policy_attachment" "lambda_schedule_policy_attachment" {
+#   policy_arn = aws_iam_policy.policy.arn
+#   role       = aws_iam_role.lambda_schedule_role.name
+# }
+
+resource "aws_iam_policy" "scheduler_invoke_lambda" {
+  name        = "scheduler_invoke_lambda"
+  description = "Allow EventBridge Scheduler to invoke Lambda functions"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "lambda:InvokeFunction",
+        Resource = [
+          aws_lambda_function.ec2_start.arn,
+          aws_lambda_function.ec2_stop.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "scheduler_invoke_lambda_attach" {
   role       = aws_iam_role.lambda_schedule_role.name
+  policy_arn = aws_iam_policy.scheduler_invoke_lambda.arn
 }
